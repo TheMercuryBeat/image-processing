6c279c2e726accb96dd56dae2fa14363c763e38d
Diego Fernandez Aceves <dfera10@gmail.com>
2021-05-29 16:20:06 +0200

    [main] Copy image from images to output

--- azure-function/ImageBlobTrigger/index.js
+++ azure-function/ImageBlobTrigger/index.js
@@ -1,3 +1,5 @@
-module.exports = async function (context, myBlob) {
-    context.log("JavaScript blob trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myBlob.length, "Bytes");
+module.exports = async function (context, inputImage) {
+    context.log("Received Blob:", context.bindingData.name, "\n");
+    context.bindings.outputImage = context.bindings.inputImage;
+    context.done();
 };
