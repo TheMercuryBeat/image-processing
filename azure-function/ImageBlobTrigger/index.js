737bbd824f054ca7277609ac3d2adc67cccc05e7
Diego Fernandez Aceves <dfera10@gmail.com>
2021-05-30 17:39:59 +0200

    [main] Consume images from blob, then process the images and upload to another container

--- azure-function/ImageBlobTrigger/index.js
+++ azure-function/ImageBlobTrigger/index.js
@@ -1,5 +1,72 @@
+const { BlobServiceClient, StorageSharedKeyCredential } = require('@azure/storage-blob');
+const sharp = require("sharp");
+const md5 = require("md5");
+
+const account = process.env.ACCOUNT_NAME;
+const accountKey = process.env.ACCOUNT_KEY;
+const containerName = process.env.OUTPUT_CONTAINER_STORAGE || 'output';
+const imageSizes = [800, 1024];
+
+const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
+const blobServiceClient = new BlobServiceClient(getBaseUrl(), sharedKeyCredential);
+
+function getBaseUrl() {
+    return `https://${account}.blob.core.windows.net`;
+}
+
+async function upload(imageInformation) {
+
+    const { name, extension, content, contentType, size } = imageInformation;
+
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    await containerClient.createIfNotExists({ access: 'blob' });
+
+    const imageResizedMd5Name = md5(content);
+    const imagePath = `${name}/${size}/${imageResizedMd5Name}.${extension}`;
+
+    const blockBlobClient = containerClient.getBlockBlobClient(imagePath);
+    const blobOptions = {
+        blobHTTPHeaders: {
+            blobContentType: contentType
+        }
+    };
+
+    const uploadBlobResponse = await blockBlobClient.upload(content, Buffer.byteLength(content), blobOptions);
+    return { uploadBlobResponse, imagePath };
+}
+
+function buildImageInformation(context, content, size) {
+
+    const contentType = context.bindingData.properties.contentType;
+    const [name, extension] = context.bindingData.name.split('.');
+
+    return { name, extension, content, contentType, size };
+
+}
+
 module.exports = async function (context, inputImage) {
-    context.log("Received Blob:", context.bindingData.name, "\n");
-    context.bindings.outputImage = context.bindings.inputImage;
-    context.done();
+
+    try {
+
+        context.log(`Received Blob: ${context.bindingData.name} \n`);
+
+        const imagesProcessing = await Promise.all(imageSizes.map(async size => {
+
+            const imageContentResized = await sharp(inputImage).resize({ width: size }).toBuffer();
+            const imageResizedInformation = buildImageInformation(context, imageContentResized, size)
+            const { uploadBlobResponse, imagePath } = await upload(imageResizedInformation);
+
+            context.log(`Upload block blob ${imagePath} successfully`, uploadBlobResponse.requestId);
+            return `${getBaseUrl()}/${containerName}/${imagePath}`;
+
+        }));
+
+        context.log(`Images processed successfully: ${imagesProcessing} \n`);
+
+    } catch (err) {
+        context.log(`Images processing failure: ${err} \n`);
+    } finally {
+        context.done();
+    }
+
 };
