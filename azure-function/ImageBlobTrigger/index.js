4396615edf787e3866884218f85d165be76670d4
Diego Fernandez Aceves <dfera10@gmail.com>
2021-05-31 18:36:45 +0200

    [main] Extract code from index.js to imageService.js

--- azure-function/ImageBlobTrigger/index.js
+++ azure-function/ImageBlobTrigger/index.js
@@ -1,85 +1,6 @@
-const { BlobServiceClient, StorageSharedKeyCredential } = require('@azure/storage-blob');
-const sharp = require("sharp");
-const md5 = require("md5");
-const sizeOf = require('image-size')
-
 const { updateTaskStatus } = require('./service/taskService');
-const { saveImage } = require('./service/imageService');
-
-const account = process.env.ACCOUNT_NAME;
-const accountKey = process.env.ACCOUNT_KEY;
-const containerName = process.env.OUTPUT_CONTAINER_STORAGE || 'output';
+const { saveOriginalImage, saveResizedImage, processAndUploadImage } = require('./service/imageService');
 const imageSizes = [800, 1024];
-
-const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
-const blobServiceClient = new BlobServiceClient(getBaseUrl(), sharedKeyCredential);
-
-function getBaseUrl() {
-    return `https://${account}.blob.core.windows.net`;
-}
-
-async function upload(imageResizedInformation) {
-
-    const { name, extension, content, contentType, size } = imageResizedInformation;
-
-    const containerClient = blobServiceClient.getContainerClient(containerName);
-    await containerClient.createIfNotExists({ access: 'blob' });
-
-    const imageResizedContentMd5 = md5(content);
-    const imageResizedRelativePath = `${name}/${size}/${imageResizedContentMd5}.${extension}`;
-
-    const blockBlobClient = containerClient.getBlockBlobClient(imageResizedRelativePath);
-    const blobOptions = {
-        blobHTTPHeaders: {
-            blobContentType: contentType
-        }
-    };
-
-    const uploadBlobResponse = await blockBlobClient.upload(content, Buffer.byteLength(content), blobOptions);
-    return {
-        requestId: uploadBlobResponse.requestId,
-        imageResizedName: `${imageResizedContentMd5}.${extension}`,
-        imageResizedAbsolutePath: `${getBaseUrl()}/${containerName}/${imageResizedRelativePath}`,
-        imageResizedContentMd5
-    };
-}
-
-async function buildImageResizedInformation(filename, contentType, originalImageContent, size) {
-
-    const [name, extension] = filename.split('.');
-    const content = await sharp(originalImageContent).resize({ width: size }).toBuffer();
-
-    return { name, extension, content, contentType, size };
-
-}
-
-function espaceQuotes(value) {
-    return value.replace(/['"]+/g, '');
-}
-
-async function saveOriginalImage(name, path, buffer) {
-
-    const md5Content = md5(buffer);
-    const { height: resolution } = sizeOf(buffer);
-    return await saveImage({ name, path, md5Content, resolution });
-
-}
-
-async function saveResizedImage(originalImage, imageResized, size) {
-
-    const { imageResizedName, imageResizedAbsolutePath, imageResizedContentMd5 } = imageResized;
-
-    const imageResizedData = {
-        name: imageResizedName,
-        path: imageResizedAbsolutePath,
-        md5Content: imageResizedContentMd5,
-        resolution: size,
-        originalId: originalImage.id
-    };
-
-    return await saveImage(imageResizedData);
-
-}
 
 
 module.exports = async function (context, inputImage) {
@@ -100,9 +21,9 @@ module.exports = async function (context, inputImage) {
 
         const imagesProcessed = await Promise.all(imageSizes.map(async size => {
 
-            const imageResizedInformation = await buildImageResizedInformation(name, contentType, inputImage, size);
-            const imageResizedUploaded = await upload(imageResizedInformation);
-            context.log(`Upload block blob ${imageResizedUploaded.imageResizedAbsolutePath} successfully ${imageResizedUploaded.requestId}`);
+            const imageData = { name, inputImage, contentType, size };
+            const imageResizedUploaded = await processAndUploadImage(imageData);
+            context.log(`Upload block blob ${imageResizedUploaded.imageResizedAbsolutePath} successfully`);
 
             const imageResizedSaved = await saveResizedImage(originalImageSaved, imageResizedUploaded, size);
             context.log(`Saved original image: ${JSON.stringify(imageResizedSaved)}`);
@@ -124,3 +45,7 @@ module.exports = async function (context, inputImage) {
     }
 
 };
+
+function espaceQuotes(value) {
+    return value.replace(/['"]+/g, '');
+}
--- azure-function/ImageBlobTrigger/service/imageService.js
+++ azure-function/ImageBlobTrigger/service/imageService.js
@@ -1,7 +1,84 @@
+const { BlobServiceClient, StorageSharedKeyCredential } = require('@azure/storage-blob');
+const sharp = require("sharp");
+const md5 = require("md5");
+const sizeOf = require('image-size')
 const { Images } = require('../model/images');
+
+const account = process.env.ACCOUNT_NAME;
+const accountKey = process.env.ACCOUNT_KEY;
+const containerName = process.env.OUTPUT_CONTAINER_STORAGE || 'output';
+
+
+const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
+const blobServiceClient = new BlobServiceClient(getBaseUrl(), sharedKeyCredential);
+
+function getBaseUrl() {
+    return `https://${account}.blob.core.windows.net`;
+}
+
+async function saveOriginalImage(name, path, buffer) {
+
+    const md5Content = md5(buffer);
+    const { height: resolution } = sizeOf(buffer);
+    return await saveImage({ name, path, md5Content, resolution });
+
+}
+
+async function saveResizedImage(originalImage, imageResized, size) {
+
+    const { imageResizedName, imageResizedAbsolutePath, imageResizedContentMd5 } = imageResized;
+
+    const imageResizedData = {
+        name: imageResizedName,
+        path: imageResizedAbsolutePath,
+        md5Content: imageResizedContentMd5,
+        resolution: size,
+        originalId: originalImage.id
+    };
+
+    return await saveImage(imageResizedData);
+
+}
+
+async function buildImageResizedInformation(imageData) {
+
+    const { name: filename, inputImage, contentType, size } = imageData
+
+    const [name, extension] = filename.split('.');
+    const content = await sharp(inputImage).resize({ width: size }).toBuffer();
+
+    return { name, extension, content, contentType, size };
+
+}
+
+async function processAndUploadImage(imageData) {
+
+    const { name, extension, content, contentType, size } = await buildImageResizedInformation(imageData);
+
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    await containerClient.createIfNotExists({ access: 'blob' });
+
+    const imageResizedContentMd5 = md5(content);
+    const imageResizedRelativePath = `${name}/${size}/${imageResizedContentMd5}.${extension}`;
+
+    const blockBlobClient = containerClient.getBlockBlobClient(imageResizedRelativePath);
+    const blobOptions = {
+        blobHTTPHeaders: {
+            blobContentType: contentType
+        }
+    };
+
+    await blockBlobClient.upload(content, Buffer.byteLength(content), blobOptions);
+    
+    return {
+        imageResizedName: `${imageResizedContentMd5}.${extension}`,
+        imageResizedAbsolutePath: `${getBaseUrl()}/${containerName}/${imageResizedRelativePath}`,
+        imageResizedContentMd5
+    };
+}
 
 async function saveImage(image) {
     return await Images.create(image);
 }
 
-module.exports = { saveImage };
+module.exports = { saveOriginalImage, saveResizedImage, processAndUploadImage };
